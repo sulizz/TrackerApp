523289173cb22457be120e14eb4bd4c241c576b3
sulizbasnet <sulizzbasnet@gmail.com>
2022-03-04 00:14:07 -0800

    intergrated ngrok

---
+++ src/api/tracker.js
@@ -0,0 +1,7 @@
+import axios from "axios";
+
+//create a instance and export it
+
+export default axios.create({
+    baseURL: "http://74ac-2600-1700-bf91-4000-7593-53cb-a918-274d.ngrok.io",
+});
--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -11,8 +11,26 @@ const authReducer = (state, action) => {
 
 //action function that calls reducer  to modify state
 const signup = (dispatch) => {
-    //inner function gets called inside our component
-    return () => {};
+    //inner function gets called inside our component.
+    return ({ email, password }) => {
+        //make api request to sign up with that email and password
+        //if we sign up, modify our state, and say that we are authenticated
+        //if sign up fails, need error message
+    };
+};
+
+const signin = (dispatch) => {
+    return ({ email, password }) => {
+        //try to sign in
+        //handle success by updating state
+        //error
+    };
+};
+
+const signout = (dispatch) => {
+    return () => {
+        //sign out
+    };
 };
 //export provier and context
 export const { Provider, Context } = createDataContext(
