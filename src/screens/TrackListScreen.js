d20830e94e9788af0ebc0e945dd4ae4c358f7ad7
sulizbasnet <sulizzbasnet@gmail.com>
2022-03-09 16:39:30 -0800

    created tracks list and tracks details page, added iconson the bottom tab

--- src/screens/TrackListScreen.js
+++ src/screens/TrackListScreen.js
@@ -1,11 +1,51 @@
-import React from "react";
-import { View, Text, StyleSheet, Button } from "react-native";
-import {} from "react-native-elements";
+import React, { useContext, useEffect } from "react";
+import {
+    View,
+    Text,
+    StyleSheet,
+    Button,
+    FlatList,
+    TouchableOpacity,
+} from "react-native";
+import { ListItem } from "react-native-elements";
+import { NavigationEvents } from "react-navigation";
+import { Context as TrackerContext } from "../context/TrackContext";
+import { navigate } from "../navigationRef";
 
 const TrackListScreen = ({ navigation }) => {
+    const { state, fetchTracks } = useContext(TrackerContext);
+
+    // useEffect(() => {
+    //     fetchTracks();
+    // }, []);
+
+    //anytime we navigate to this page we need to fetch the data
+    // console.log(state);
     return (
         <View>
-            <Text>TrackListScreen</Text>
+            <NavigationEvents onWillFocus={() => fetchTracks()} />
+            <FlatList
+                data={state}
+                keyExtractor={(item) => item._id}
+                renderItem={({ item }) => {
+                    return (
+                        <TouchableOpacity
+                            onPress={() =>
+                                navigation.navigate("TrackDetails", {
+                                    _id: item._id,
+                                })
+                            }
+                        >
+                            <ListItem>
+                                <ListItem.Content>
+                                    <ListItem.Title>{item.name}</ListItem.Title>
+                                </ListItem.Content>
+                                <ListItem.Chevron />
+                            </ListItem>
+                        </TouchableOpacity>
+                    );
+                }}
+            />
             <Button
                 title="Tack details"
                 onPress={() => navigation.navigate("TrackDetails")}
@@ -13,5 +53,12 @@ const TrackListScreen = ({ navigation }) => {
         </View>
     );
 };
+
+TrackListScreen.navigationOptions = () => {
+    return {
+        title: "Your Tracks",
+    };
+};
+
 const styles = StyleSheet.create({});
 export default TrackListScreen;
